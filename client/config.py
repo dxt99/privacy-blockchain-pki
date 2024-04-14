3d7ad3bc75d7089b7dfe9444eb4224ed16655670
Frederik Imanuel Louis <frederik.louis@unit4.com>
2024-04-14 21:34:49 +0700

    feat: added base url conf

--- client/config.py
+++ client/config.py
@@ -2,3 +2,4 @@ import os
 
 # Flask settings
 flask_host = os.environ['host'] if 'host' in os.environ else '127.0.0.1'
+ca_base_url = os.environ['ca_url'] if 'ca_url' in os.environ else 'http://localhost:8080'
