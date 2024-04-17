559d41dd5e0e2ed248bb839a36aabd52f2ce32dc
dxt99 <ericlouis19x@gmail.com>
2024-04-17 19:23:38 +0700

    feat: hitting ca from client

--- client/app.py
+++ client/app.py
@@ -1,5 +1,6 @@
 import connexion
 import config
+import requests
 from pathlib import Path
 from connexion.options import SwaggerUIOptions
 
@@ -8,8 +9,19 @@ options = SwaggerUIOptions(swagger_ui_path="/swagger")
 def hello():
     return "hello"
 
-def hello2():
-    return "hi"
+def ca_status(transaction: str):
+    url = f"{config.ca_base_url}/status"
+    res = requests.post(url, json=transaction)
+    try:
+        payload = res.json()
+        headers = {"Content-Type": "application/json"}
+        return payload, 200, headers
+    except:
+        return connexion.problem(
+            title = "Bad request",
+            detail = res.text,
+            status = res.status_code,
+        )
 
 app = connexion.FlaskApp(__name__, swagger_ui_options=options, specification_dir="spec")
 app.add_api('openapi.yaml')
