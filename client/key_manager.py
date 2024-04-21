8be5a1dca4944208010bcd64ba18e9598c9bc77b
dxt99 <ericlouis19x@gmail.com>
2024-04-21 19:22:01 +0700

    feat: added basic key generation

--- client/key_manager.py
+++ client/key_manager.py
@@ -1,11 +1,32 @@
-from Cryptodome.Random import random
-
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from typing import List
 class KeyManager:
-    online_master_key: int
-    offline_master_key: int
+    public_key_exponent: int = 0x10001
+    key_size: int = 2048
+    master_key: rsa.RSAPrivateKey
+    online_keys: List[rsa.RSAPrivateKey]
+    offline_keys: List[rsa.RSAPublicKey]
     
     def __init__(self):
         pass
     
-    def new_key(self):
-        random.getrandbits(512)
+    def __gen_generic_key(self):
+        return rsa.generate_private_key(self.public_key_exponent,  self.key_size)
+    
+    @staticmethod
+    def public_key_str(key: rsa.RSAPrivateKey):
+        return key.public_key().public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.PKCS1)
+    
+    def new_key_chain(self):
+        self.master_key = self.__gen_generic_key()
+        self.online_keys = [self.__gen_generic_key()]
+        self.offline_keys = [self.__gen_generic_key()]
+        
+    def add_key_to_chain(self):
+        raise NotImplementedError
+        
+if __name__ == '__main__':
+    key = rsa.generate_private_key(0x10001, 2048)
+    print(key.public_key().public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo))
+    print(key.public_key().public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.PKCS1))
