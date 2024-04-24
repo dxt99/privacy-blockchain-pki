56c7001a09b6bcdf2ca84177e9eab8f2941a3ae9
dxt99 <ericlouis19x@gmail.com>
2024-04-24 23:35:08 +0700

    feat: client update

--- client/key_manager.py
+++ client/key_manager.py
@@ -6,8 +6,8 @@ class KeyManager:
     public_key_exponent: int = 0x10001
     key_size: int = 2048
     master_key: rsa.RSAPrivateKey
+    base_key: rsa.RSAPrivateKey
     online_keys: List[rsa.RSAPrivateKey]
-    offline_keys: List[rsa.RSAPublicKey]
     
     def __init__(self):
         pass
@@ -20,6 +20,14 @@ class KeyManager:
         return key.public_key().public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.PKCS1).hex()
     
     @staticmethod
+    def encrypt(key: rsa.RSAPublicKey, payload: bytes | str) -> bytes:
+        encoded_payload = payload if type(payload) == bytes else payload.encode()
+        return key.encrypt(
+            encoded_payload, 
+            padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH)
+        )
+    
+    @staticmethod
     def sign(key: rsa.RSAPrivateKey, payload: bytes | str) -> bytes:
         encoded_payload = payload if type(payload) == bytes else payload.encode()
         return key.sign(
@@ -30,14 +38,14 @@ class KeyManager:
     
     def new_key_chain(self):
         self.master_key = self.__gen_generic_key()
-        self.online_keys = [self.__gen_generic_key()]
-        self.offline_keys = [self.__gen_generic_key()]
+        self.base_key = self.__gen_generic_key()
         
     def add_key_to_chain(self):
-        raise NotImplementedError
+        new_key = self.__gen_generic_key()
+        self.online_keys.append(new_key)
+        return new_key
         
 if __name__ == '__main__':
     key = rsa.generate_private_key(0x10001, 2048)
     print(key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption()))
     print(key.public_key().public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.PKCS1))
-    #print(KeyManager.sign(key, b"data"))
--- client/transaction_service.py
+++ client/transaction_service.py
@@ -1,28 +1,45 @@
 import config
 from key_manager import KeyManager
 from ca_service import CaService
+from chain_service import ChainService
 from model import Transaction
-from typing import List
+from typing import Dict
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 class TransactionService:
-    name: str
+    chain_service: ChainService
     key_manager: KeyManager
+    
+    name: str
     register_transaction: Transaction = None
     register_transaction_id: int = -1
+    update_transactions: Dict[int, Transaction]
+    verifier_key: rsa.RSAPublicKey = None
 
     def __init__(self):
         self.name = config.client_common_name
         self.key_manager = KeyManager()
+        self.chain_service = ChainService()
     
     @staticmethod
     def __serialize_signatures(*signatures: bytes):
         return f'({",".join([i.hex() for i in signatures])})'
     
+    def __get_verifier_key(self):
+        if self.verifier_key != None: return
+        try: 
+            transaction = self.chain_service.get_transaction(config.verifier_tx_id)
+            self.verifier_key = serialization.load_pem_public_key(bytes.fromhex(transaction.public_key))
+        except Exception as e:
+            print("Failed to parse verifier public key")
+            raise e
+    
     def generate_and_register(self) -> bool:
         self.register_transaction = None
         self.key_manager.new_key_chain()
         
-        online_key = self.key_manager.online_keys[0]
+        online_key = self.key_manager.base_key
         master_key = self.key_manager.master_key
         online_signature: bytes = KeyManager.sign(online_key, self.name)
         master_signature: bytes = KeyManager.sign(master_key, self.name)
@@ -37,6 +54,30 @@ class TransactionService:
         res = CaService.register_transaction(register_transaction)
         if res: self.register_transaction = register_transaction
         return res
+    
+    def update_key(self) -> bool:
+        self.__get_verifier_key()
+        if not self.register_transaction:
+            print("Cannnot udpate key, no registration found")
+            return False
+        
+        new_key = self.key_manager.add_key_to_chain()
+        public_key = KeyManager.public_key_str(new_key)
+        prev_data = self.register_transaction.public_key
+        
+        previous_signature: bytes = KeyManager.sign(new_key, prev_data)
+        encrypted_signature: bytes = KeyManager.encrypt(self.verifier_key, previous_signature)
+        online_signature: bytes = KeyManager.sign(new_key, public_key)
+        
+        signatures: str = self.__serialize_signatures(online_signature, encrypted_signature)
+        update_transaction = Transaction(
+            identity = "",
+            public_key = public_key,
+            signatures = signatures
+        )
+        id = self.chain_service.update(update_transaction)
+        self.update_transactions[id] = update_transaction
+        return True
     
     def register_request_status(self) -> str:
         if self.register_transaction == None:
